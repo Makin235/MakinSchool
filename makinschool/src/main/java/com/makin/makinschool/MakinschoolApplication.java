4e7eddc4467d7e3d72e7fa97d8584e2b01c6e709
Makin235 <abdelhamid.makin@gmail.com>
2024-07-28 15:29:51 +0300

    Initial Commit Spring Boot Prj

---
+++ makinschool/src/main/java/com/makin/makinschool/MakinschoolApplication.java
@@ -0,0 +1,13 @@
+package com.makin.makinschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakinschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakinschoolApplication.class, args);
+	}
+
+}
---
+++ makinschool/src/test/java/com/makin/makinschool/MakinschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makin.makinschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakinschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
