0c0bb84547e5d9c392c517fdc0d1e99de1627ab9
Makin235 <abdelhamid.makin@gmail.com>
2024-08-03 16:12:31 +0300

    Server side form validation applied.

--- makinschool/src/main/java/com/makin/makinschool/controller/ContactPageController.java
+++ makinschool/src/main/java/com/makin/makinschool/controller/ContactPageController.java
@@ -2,10 +2,15 @@ package com.makin.makinschool.controller;
 
 import com.makin.makinschool.model.ContactModel;
 import com.makin.makinschool.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +19,7 @@ import javax.swing.*;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+@Slf4j
 @Controller
 public class ContactPageController {
 
@@ -25,13 +31,18 @@ public class ContactPageController {
     }
 
     @RequestMapping("/contact")
-    public String displayContactPage() {
+    public String displayContactPage(Model model) {
+        model.addAttribute("contact", new ContactModel());
         return "contact";
     }
 
     @RequestMapping(value = "/saveMsg", method = POST)
-    public ModelAndView saveMessage(ContactModel contactModel) {
-        contactService.saveMessageDetails(contactModel);
-        return new ModelAndView("redirect:/contact");
+    public String saveMessage(@Valid @ModelAttribute("contact") ContactModel contact, Errors errors) {
+        if (errors.hasErrors()) {
+            log.error("Contact form validation failed due to: " + errors.toString());
+            return "contact";
+        }
+        contactService.saveMessageDetails(contact);
+        return "redirect:/contact";
     }
 }
--- makinschool/src/main/java/com/makin/makinschool/model/ContactModel.java
+++ makinschool/src/main/java/com/makin/makinschool/model/ContactModel.java
@@ -1,13 +1,26 @@
 package com.makin.makinschool.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class ContactModel {
 
+    @NotBlank(message = "Name must not be blank.")
+    @Size(min = 2, message = "Name must be at least 2 characters long.")
     private String name;
+
+    @NotBlank(message = "Mobile number must not be blank.")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits.")
     private String mobileNum;
+
+    @NotBlank(message = "Email must not be blank.")
+    @Email(message = "Please provide a valid email.")
     private String email;
+
+    @NotBlank(message = "Subject must not be blank.")
     private String subject;
+
+    @NotEmpty(message = "Subject must not be empty.")
     private String message;
 }
