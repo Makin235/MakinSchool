f5edd417cd34e0fc9f62da475d297716f2744a5c
Makin235 <abdelhamid.makin@gmail.com>
2024-08-03 12:00:01 +0300

    Usage of RequestParam

--- makinschool/src/main/java/com/makin/makinschool/controller/HolidayPageController.java
+++ makinschool/src/main/java/com/makin/makinschool/controller/HolidayPageController.java
@@ -3,7 +3,9 @@ package com.makin.makinschool.controller;
 import com.makin.makinschool.model.HolidayModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,8 +14,11 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidayPageController {
 
-    @RequestMapping("/holidays")
-    public String displayHolidaysPage(Model model) {
+    @GetMapping("/holidays")
+    public String displayHolidaysPage(
+            @RequestParam(required = false) boolean festival,
+            @RequestParam(required = false) boolean federal,
+            Model model) {
         List<HolidayModel> holidays = Arrays.asList(
                 new HolidayModel("July 4", "Independence Day", HolidayModel.Type.FEDERAL),
                 new HolidayModel("Sep 5", "Labor Day", HolidayModel.Type.FEDERAL),
@@ -29,6 +34,8 @@ public class HolidayPageController {
         }
 
         model.addAttribute("appName", "Makin School");
+        model.addAttribute("festival", festival);
+        model.addAttribute("federal", federal);
         return "holidays";
     }
 }
