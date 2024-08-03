5b65a93835b4a96177a3186bdf6dbf1a2523afb3
Makin235 <abdelhamid.makin@gmail.com>
2024-08-03 21:03:51 +0300

    Disabling csrf authentication.

--- makinschool/src/main/java/com/makin/makinschool/config/ProjectSecurityConfig.java
+++ makinschool/src/main/java/com/makin/makinschool/config/ProjectSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,16 +18,18 @@ public class ProjectSecurityConfig {
 //                .formLogin(Customizer.withDefaults())
 //                .httpBasic(Customizer.withDefaults());
 
-        http.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/","/home").permitAll()
-                        .requestMatchers("/about").permitAll()
-                        .requestMatchers("/courses").denyAll()
-                        .requestMatchers("/holidays/**").permitAll()
-                        .requestMatchers("/contact").authenticated()
-                        .requestMatchers("/assets/**").permitAll()
-                )
-                .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults());
+        http
+            .csrf(csrf -> csrf.disable()) // Disable CSRF protection
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/", "/home").permitAll()
+                    .requestMatchers("/about").permitAll()
+                    .requestMatchers("/courses").denyAll()
+                    .requestMatchers("/holidays/**").permitAll()
+                    .requestMatchers("/contact").authenticated()
+                    .requestMatchers("/assets/**").permitAll()
+            )
+            .formLogin(Customizer.withDefaults()) // Enable form-based login with default settings
+            .httpBasic(Customizer.withDefaults()); // Enable HTTP Basic authentication with default settings
 
         return http.build();
     }
