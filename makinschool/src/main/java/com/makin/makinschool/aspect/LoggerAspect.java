27249a3bb0f23d4884212df978180f5f4a898186
Makin235 <abdelhamid.makin@gmail.com>
2024-08-12 20:29:06 +0300

    Implementation of AOP.

---
+++ makinschool/src/main/java/com/makin/makinschool/aspect/LoggerAspect.java
@@ -0,0 +1,38 @@
+package com.makin.makinschool.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.Instant;
+
+@Component
+@Aspect
+@Slf4j
+public class LoggerAspect {
+
+    @Around("execution(* com.makin.makinschool..*.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info(joinPoint.getSignature().toString() + " method execution start.");
+        log.info(" ************************************[ Start ]************************************");
+        Instant start = Instant.now();
+        Object returnObj = joinPoint.proceed();
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();
+        log.info("Time took to execute " + joinPoint.getSignature().toString() + " method is: " + timeElapsed + ".");
+        log.info(" ************************************[ End ]************************************");
+        log.info(joinPoint.getSignature().toString() + " method execution end.");
+        return returnObj;
+    }
+
+    @AfterThrowing(value = "execution(* com.makin.makinschool..*.*(..))", throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        log.error(joinPoint.getSignature() + " An exception occurred due to: " + ex.getMessage());
+
+    }
+}
