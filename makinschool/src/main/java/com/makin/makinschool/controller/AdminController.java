f18686e5166ac68a79ce756be69a823208129eb6
Makin235 <abdelhamid.makin@gmail.com>
2024-08-21 20:22:51 +0300

    display class students, add, delete student functionalities are added.

--- makinschool/src/main/java/com/makin/makinschool/controller/AdminController.java
+++ makinschool/src/main/java/com/makin/makinschool/controller/AdminController.java
@@ -1,11 +1,14 @@
 package com.makin.makinschool.controller;
 
 import com.makin.makinschool.model.MakinClass;
+import com.makin.makinschool.model.Person;
 import com.makin.makinschool.service.MakinClassService;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -39,7 +42,7 @@ public class AdminController {
         return modelAndView;
     }
 
-    @RequestMapping("/addNewClass")
+    @PostMapping("/addNewClass")
     public ModelAndView addNewClass(@ModelAttribute("makinClass") MakinClass makinClass) {
         makinClassService.addClass(makinClass);
         ModelAndView modelAndView = new ModelAndView("redirect:/admin/displayClasses");
@@ -50,6 +53,46 @@ public class AdminController {
     public ModelAndView deleteClass(@RequestParam int id) {
         makinClassService.deleteClass(id);
         ModelAndView modelAndView = new ModelAndView("redirect:/admin/displayClasses");
+        return modelAndView;
+    }
+
+    @RequestMapping("/displayStudents")
+    public ModelAndView displayStudents(@RequestParam int classId, HttpSession session,
+                                        @RequestParam(value = "error", required = false) String error) {
+        String errorMsg = null;
+        MakinClass makinClass = makinClassService.findClass(classId);
+        ModelAndView modelAndView = new ModelAndView("students");
+        modelAndView.addObject("makinClass", makinClass);
+        modelAndView.addObject("person", new Person());
+        modelAndView.addObject("appName", "Makin School");
+        session.setAttribute("makinClass", makinClass);
+        if (null != error) {
+            errorMsg = "Invalid email entered!";
+            modelAndView.addObject("errorMessage", errorMsg);
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping("/addStudent")
+    public ModelAndView addStudent(@ModelAttribute("person") Person person, HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView();
+        MakinClass makinClass = (MakinClass) session.getAttribute("makinClass");
+        boolean isSaved = makinClassService.addStudent(makinClass, person, session);
+        if (isSaved) {
+            modelAndView.setViewName("redirect:/admin/displayStudents?classId=" + makinClass.getClassId());
+        } else {
+            modelAndView.setViewName("redirect:/admin/displayStudents?classId=" +
+                    makinClass.getClassId() + "&error=true");
+        }
+        return modelAndView;
+    }
+
+    @RequestMapping("/deleteStudent")
+    public ModelAndView deleteStudent(@RequestParam int personId, HttpSession session) {
+        ModelAndView modelAndView = new ModelAndView();
+        MakinClass makinClass = (MakinClass) session.getAttribute("makinClass");
+        makinClassService.deleteStudent(personId, makinClass, session);
+        modelAndView.setViewName("redirect:/admin/displayStudents?classId=" + makinClass.getClassId());
         return modelAndView;
     }
 }
--- makinschool/src/main/java/com/makin/makinschool/service/MakinClassService.java
+++ makinschool/src/main/java/com/makin/makinschool/service/MakinClassService.java
@@ -4,6 +4,7 @@ import com.makin.makinschool.model.MakinClass;
 import com.makin.makinschool.model.Person;
 import com.makin.makinschool.repository.MakinClassRepository;
 import com.makin.makinschool.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -42,5 +43,25 @@ public class MakinClassService {
     public MakinClass findClass(int classId) {
         Optional<MakinClass> makinClass = makinClassRepository.findById(classId);
         return makinClass.get();
+    }
+
+    public boolean addStudent(MakinClass makinClass, Person person, HttpSession session) {
+        boolean isAdded = false;
+        Person personEntity = personRepository.readByEmail(person.getEmail());
+        if (null != personEntity && personEntity.getPersonId() > 0) {
+            personEntity.setMakinClass(makinClass);
+            personRepository.save(personEntity);
+            makinClass.getPersons().add(personEntity);
+            makinClassRepository.save(makinClass);
+            isAdded = true;
+        }
+        return isAdded;
+    }
+    public void deleteStudent(int studentId, MakinClass makinClass, HttpSession session) {
+        Optional<Person> person = personRepository.findById(studentId);
+        person.get().setMakinClass(null);
+        makinClass.getPersons().remove(person.get());
+        MakinClass savedMakinClass = makinClassRepository.save(makinClass);
+        session.setAttribute("makinClass", savedMakinClass);
     }
 }
