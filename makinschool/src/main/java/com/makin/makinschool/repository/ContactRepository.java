ca897b17f50120dc28fb3ee1cabd642b989910a4
Makin235 <abdelhamid.makin@gmail.com>
2024-08-24 14:29:34 +0300

    Implementation of custom query.

--- makinschool/src/main/java/com/makin/makinschool/repository/ContactRepository.java
+++ makinschool/src/main/java/com/makin/makinschool/repository/ContactRepository.java
@@ -4,6 +4,7 @@ import com.makin.makinschool.model.ContactModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,5 +13,8 @@ import java.util.List;
 public interface ContactRepository extends JpaRepository<ContactModel, Integer> {
 
     List<ContactModel> findByStatus(String status);
+
+    //@Query("SELECT c FROM ContactModel c WHERE c.status = :status") //JPQL
+    @Query(value = "SELECT * FROM contact_msg c WHERE c.status = :status", nativeQuery = true) //Native SQL
     Page<ContactModel> findByStatus(String status, Pageable pageable);
 }
