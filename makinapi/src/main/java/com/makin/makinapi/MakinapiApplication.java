e95ee0f11ca5dd5d16348c5a83036fb2f0381148
Makin235 <abdelhamid.makin@gmail.com>
2024-08-25 18:47:21 +0300

    MakinAPI project is added.

---
+++ makinapi/src/main/java/com/makin/makinapi/MakinapiApplication.java
@@ -0,0 +1,13 @@
+package com.makin.makinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakinapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakinapiApplication.class, args);
+	}
+
+}
---
+++ makinapi/src/test/java/com/makin/makinapi/MakinapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makin.makinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakinapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
