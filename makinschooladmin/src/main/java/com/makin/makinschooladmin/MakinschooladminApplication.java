a5e7d102225ea9513709944152ff1152da6b935c
Makin235 <abdelhamid.makin@gmail.com>
2024-08-31 18:55:50 +0300

    MakinSchool Admin project is added.

---
+++ makinschooladmin/src/main/java/com/makin/makinschooladmin/MakinschooladminApplication.java
@@ -0,0 +1,13 @@
+package com.makin.makinschooladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakinschooladminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakinschooladminApplication.class, args);
+	}
+
+}
